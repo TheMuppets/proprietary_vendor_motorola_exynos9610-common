8e720a6516ad6e5140f8fc2f8ece096d8b7e9e55
Nolen Johnson <johnsonnolen@gmail.com>
2024-10-27 16:35:21 -0400

    exynos9610-common: Move libdemange shim to prebuilt
    * Refuses to build in Android 15.

--- exynos9610-common-vendor.mk
+++ exynos9610-common-vendor.mk
@@ -280,6 +280,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/exynos9610-common/proprietary/vendor/lib/libaudio-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaudio-ril.so \
     vendor/motorola/exynos9610-common/proprietary/vendor/lib/libaudioproxy.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaudioproxy.so \
     vendor/motorola/exynos9610-common/proprietary/vendor/lib/libdapparamstorage.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdapparamstorage.so \
+    vendor/motorola/exynos9610-common/proprietary/vendor/lib/libdemangle.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdemangle.so \
     vendor/motorola/exynos9610-common/proprietary/vendor/lib/libexynoscamera_hifi_plugin.so:$(TARGET_COPY_OUT_VENDOR)/lib/libexynoscamera_hifi_plugin.so \
     vendor/motorola/exynos9610-common/proprietary/vendor/lib/libexynoscamera_hifills_plugin.so:$(TARGET_COPY_OUT_VENDOR)/lib/libexynoscamera_hifills_plugin.so \
     vendor/motorola/exynos9610-common/proprietary/vendor/lib/libexynoscamera_plugin.so:$(TARGET_COPY_OUT_VENDOR)/lib/libexynoscamera_plugin.so \
@@ -327,6 +328,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/exynos9610-common/proprietary/vendor/lib64/libadropbox.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libadropbox.so \
     vendor/motorola/exynos9610-common/proprietary/vendor/lib64/libcharon.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcharon.so \
     vendor/motorola/exynos9610-common/proprietary/vendor/lib64/libdapparamstorage.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libdapparamstorage.so \
+    vendor/motorola/exynos9610-common/proprietary/vendor/lib64/libdemangle.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libdemangle.so \
     vendor/motorola/exynos9610-common/proprietary/vendor/lib64/libexynoscamera_hifi_plugin.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libexynoscamera_hifi_plugin.so \
     vendor/motorola/exynos9610-common/proprietary/vendor/lib64/libexynoscamera_hifills_plugin.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libexynoscamera_hifills_plugin.so \
     vendor/motorola/exynos9610-common/proprietary/vendor/lib64/libexynoscamera_plugin.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libexynoscamera_plugin.so \
